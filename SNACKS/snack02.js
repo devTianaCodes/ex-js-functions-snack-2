6a4c1ad2044f140b998d77996984d278a780c7ee
SwitchTia <oblasser.tatiana@gmail.com>
2026-04-04 16:42:03 +0200

    snack 02

---
+++ SNACKS/snack02.js
@@ -0,0 +1,6 @@
+// Definisci una funzione chiamata quadrato che accetta un numero 
+// e restituisce il suo quadrato in una sola riga.
+
+const quadrato = (numero) => numero * numero;
+
+  console.log(quadrato(4)); // 16
