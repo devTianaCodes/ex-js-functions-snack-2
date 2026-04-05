158b59059d5bb551b2a5125e32f1bc62de8f9fc9
SwitchTia <oblasser.tatiana@gmail.com>
2026-04-05 11:33:38 +0200

    bonus 2

---
+++ SNACKS/bonus02.js
@@ -0,0 +1,27 @@
+// Scrivi una funzione sequenzaOperazioni che accetta un array di operazioni (funzioni) e un tempo di intervallo.
+
+// Ogni operazione deve essere eseguita in sequenza con un ritardo uguale al tempo di intervallo.
+
+
+function sequenzaOperazioni(operazioni, intervallo) {
+    for (let i = 0; i < operazioni.length; i++) {
+      setTimeout(function () {
+        operazioni[i]();
+      }, intervallo * i);
+    }
+  }
+
+ 
+  const operazioni = [
+    function () {
+      console.log("Operazione 1");
+    },
+    function () {
+      console.log("Operazione 2");
+    },
+    function () {
+      console.log("Operazione 3");
+    }
+  ];
+
+  sequenzaOperazioni(operazioni, 2000);
